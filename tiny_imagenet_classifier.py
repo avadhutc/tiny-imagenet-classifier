cd2cd928a982e68c6f97cb4aeb7b75f9c2f67105
miquel <miquelmr@kth.se>
2016-05-25 03:35:14 +0200

    Some changes

--- tiny_imagenet_classifier.py
+++ tiny_imagenet_classifier.py
@@ -108,24 +108,24 @@ for loss_function in loss_functions:
 
 
     datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
+        featurewise_center=True,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        featurewise_std_normalization=True,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=True,  # apply ZCA whitening
-        rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0.,  # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
-        vertical_flip=True)  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
 
     fpath = 'loss-' + loss_function + '-' + str(num_classes)
     datagen.fit(X_train)
 
-    df=datagen.flow(X_train, Y_train, batch_size=batch_size, shuffle=True)
+    # df=datagen.flow(X_train, Y_train, batch_size=batch_size, shuffle=True)
     
-    model.fit_generator(df,
+    model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size, shuffle=True),
                 samples_per_epoch=num_samples, nb_epoch=nb_epoch,
                 verbose=1, validation_data=(X_test,Y_test), #df,nb_val_samples=len(X_train)-num_samples,
                 callbacks=[Plotter(show_regressions=False, save_to_filepath=fpath, show_plot_window=False)])
